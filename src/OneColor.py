11ad0f83f7027ddc94d7e4af422a1c0ea72e9fc9
Mowteam <Whrosenberg9@gmail.com>
2021-03-22 21:03:43 -0700

    added file that picks one color

--- src/OneColor.py
+++ src/OneColor.py
@@ -1,15 +1,15 @@
 import board
 import neopixel
-import sys
 
-#initialize
+#initialize pixels
 pixel_pin = board.D18
-num_pixels = sys.argv[1]
+num_pixels = 149
 ORDER = neopixel.RGB 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER
 )
 
-#turn LEDs off
-pixels.fill(0, 0, 0)
+i = input("Enter a color (as R, G, B): ")
+color = tuple(int(x) for x in i.split(","))
+pixels.fill(color)
 pixels.show()
--- src/Rainbow_Runner.py
+++ src/Rainbow_Runner.py
@@ -7,7 +7,7 @@ import numpy as np
 #initialize pixels
 pixel_pin = board.D18
 num_pixels = 149
-ORDER = neopixel.GRB 
+ORDER = neopixel.RGB 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER
 )
