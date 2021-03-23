55c2ec96041837b8496d66ec4251aa592ff79f11
Mowteam <Whrosenberg9@gmail.com>
2021-03-22 21:07:11 -0700

    fixed small mistakes in OFF program

--- src/OFF.py
+++ src/OFF.py
@@ -4,12 +4,12 @@ import sys
 
 #initialize
 pixel_pin = board.D18
-num_pixels = sys.argv[1]
+num_pixels = int(sys.argv[1])
 ORDER = neopixel.RGB 
 pixels = neopixel.NeoPixel(
     pixel_pin, num_pixels, auto_write=False, pixel_order=ORDER
 )
 
 #turn LEDs off
-pixels.fill(0, 0, 0)
+pixels.fill((0, 0, 0))
 pixels.show()
