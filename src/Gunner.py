ef351dd29c79ba336897f56ffe2e19faa86d8b6b
mowteam <whrosenberg9@gmail.com>
2021-08-01 12:06:43 -0700

    reworked Gunner.py

--- src/Gunner.py
+++ src/Gunner.py
@@ -5,33 +5,30 @@ import time
 from randomColorRainbow import random_color
 from settings import num_pixels, pixels
 
-def main():
-	#make all red
-	background = [255, 0, 0]
+def shoot():
+	#make all one color
+	background = random_color()
 	pixels.fill(background)
 	pixels.show()
 
 	#initialize shooter
+	length = 1 #length of bullet (LEDs)
 	color = random_color()
-	pixels[0] = color
+	change_color(0, length, color)
 
 	#shoot pixel
-	i = num_pixels
-	while True:
-		pixels[1] =  color
+	for i in range(length, i):
+		change_color(i, length, color) #move bullet
+		change_color(i - length, length, background) #delete path of bullet
 		pixels.show()
 		time.sleep(0.05)
 
-		#shoot pixel
-		for i in range(2, i):
-			pixels[i] = color
-			pixels[i-1] = background
-			pixels.show()
-			time.sleep(0.05)
+def change_color(index, length, color):
+	for i in range(index, index + length):
+		pixels[i] = color
 
-		#reduce range
-		i--
-main()
+while True:
+	shoot()
 
 
 
